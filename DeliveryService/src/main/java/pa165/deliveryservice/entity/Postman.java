7e533f4ebc909d21560f36cb8a3d7c4fca2ad2b7
macinos <macinoss@gmail.com>
2014-10-08 21:30:45 +0200

    Postman + PostmanDao

--- DeliveryService/src/main/java/pa165/deliveryservice/entity/Postman.java
+++ DeliveryService/src/main/java/pa165/deliveryservice/entity/Postman.java
@@ -1,15 +1,32 @@
 package pa165.deliveryservice.entity;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+/**
+ * Represents a delivery service employee who takes deliveries to specified address.
+ * 
+ * @author Martin Nekula
+ */
+@Entity
 public class Postman {
 
+    @Id
+    @GeneratedValue
     private long Id;
+
     private String firstName;
+
     private String lastName;
+
+    @OneToMany(mappedBy = "postman")
     private List<Delivery> deliveries;
 
-    public Postman() {}
+    public Postman() {
+    }
 
     public long getId() {
         return Id;
@@ -43,5 +60,31 @@ public class Postman {
         this.deliveries = deliveries;
     }
 
-        
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + (int) (this.Id ^ (this.Id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Postman other = (Postman) obj;
+        if (this.Id != other.Id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Postman{" + "Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", deliveries=" + deliveries + '}';
+    }
+
 }
