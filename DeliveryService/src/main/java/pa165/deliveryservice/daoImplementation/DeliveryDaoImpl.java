03f1f77931afdd2d2455ecbe55149446322f80a6
John <janxstastny@gmail.com>
2014-10-09 16:47:37 +0200

    Changes: DeliveryDao interface Implementation of: DeliveryDaoImpl, Delivery Entity

--- DeliveryService/src/main/java/pa165/deliveryservice/daoImplementation/DeliveryDaoImpl.java
+++ DeliveryService/src/main/java/pa165/deliveryservice/daoImplementation/DeliveryDaoImpl.java
@@ -1,29 +1,83 @@
 package pa165.deliveryservice.daoImplementation;
 
+import java.util.ArrayList;
 import pa165.deliveryservice.daoInterface.DeliveryDao;
 import pa165.deliveryservice.entity.Delivery;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+/**
+ * An implementation of DeliveryDao interface
+ * @author Jan Šťastný
+ */
 public class DeliveryDaoImpl implements DeliveryDao{
-
-    public DeliveryDaoImpl() {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
-    }
-
-    public List<Delivery> getAllDelivery() {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
-    }
-
-    public void updateDelivery(Delivery delivery) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
-    }
-
-    public void deleteDelivery(Delivery delivery) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
-    }
-
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+//    public DeliveryDaoImpl() {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myUnit");
+//        entityManager = emf.createEntityManager();
+//    }
+    
+    @Override
     public void addDelivery(Delivery delivery) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+        evaluateDelivery(delivery);
+        entityManager.persist(delivery);
+        entityManager.flush();
+        entityManager.detach(delivery);
+    }
+    
+    @Override
+    public void updateDelivery(Delivery delivery) {
+        evaluateDelivery(delivery);
+        entityManager.merge(delivery);
+        entityManager.flush();        
+        entityManager.detach(delivery);
+    }
+    
+    @Override
+    public void deleteDelivery(Long id) {
+        if(id == null && id < 0) {
+            throw new IllegalArgumentException("Invalid id.");
+        }
+        Delivery delivery = entityManager.find(Delivery.class, id);
+        entityManager.remove(delivery);
+    }
+    
+    @Override
+    public Delivery getDelivery(Long id) {
+        if(id == null && id < 0) {
+            throw new IllegalArgumentException("Invalid id (id null or < 0).");
+        }
+        Delivery delivery = entityManager.find(Delivery.class, id);
+        entityManager.detach(delivery);
+        return delivery;
+    }
+    
+    @Override
+    public List<Delivery> getAllDeliveries() {
+        return entityManager.createQuery("SELECT d from Delivery d",Delivery.class).getResultList();
+    }
+    
+    /**
+     * Evaluates given delivery.
+     * @param delivery
+     * @throws IllegalArgumentException 
+     */
+    private void evaluateDelivery(Delivery delivery) throws IllegalArgumentException {
+        if(delivery == null) {
+            throw new IllegalArgumentException("Delivery can not be null!");
+        }
+        if(delivery.getId() != null && delivery.getId() < 0) {
+            throw new IllegalArgumentException("Invalid id, value < 0.");
+        }
+        if(delivery.getName().length() == 0 || delivery.getName() == null || delivery.getName().length() > Delivery.NAME_LENGTH) {
+            throw new IllegalArgumentException("Invalid name of delivery.");
+        }
     }
 
 }
--- DeliveryService/src/main/java/pa165/deliveryservice/entity/Delivery.java
+++ DeliveryService/src/main/java/pa165/deliveryservice/entity/Delivery.java
@@ -1,24 +1,40 @@
 package pa165.deliveryservice.entity;
 
 import java.util.List;
+import java.util.Objects;
+import javax.persistence.*;
 
+/**
+ * Delivery entity
+ * @author Jan Šťastný
+ */
+@Entity
 public class Delivery {
-
-    private long Id;
+    
+    public static final int NAME_LENGTH = 13;
+    @Id
+    @GeneratedValue
+    private Long Id;    
+    @Column(nullable=false,length=NAME_LENGTH)
     private String name;
+    @ManyToOne
     private Postman postman;
-    private List<Goods> packages;
+    @OneToMany(mappedBy="deliveryId")
+    private List<Goods> packages;    
+    @ManyToOne
     private Customer customer;
+    @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
-
+    
+    
     public Delivery() {
     }
 
-    public long getId() {
+    public Long getId() {
         return Id;
     }
 
-    public void setId(long Id) {
+    public void setId(Long Id) {
         this.Id = Id;
     }
 
@@ -61,5 +77,56 @@ public class Delivery {
     public void setStatus(DeliveryStatus status) {
         this.status = status;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Delivery other = (Delivery) obj;
+        if (!Objects.equals(this.Id, other.Id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.postman, other.postman)) {
+            return false;
+        }
+        if (!Objects.equals(this.packages, other.packages)) {
+            return false;
+        }
+        if (!Objects.equals(this.customer, other.customer)) {
+            return false;
+        }
+        if (this.status != other.status) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.Id);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.postman);
+        hash = 71 * hash + Objects.hashCode(this.packages);
+        hash = 71 * hash + Objects.hashCode(this.customer);
+        hash = 71 * hash + (this.status != null ? this.status.hashCode() : 0);
+        return hash;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Delivery["+ Id.toString() + "] " + name + ", P:" + postman + ", G:{" + packages + "}, C:" + customer + ", S:" + status;
+    }
+    
+    
 
 }
