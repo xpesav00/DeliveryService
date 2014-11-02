9722ee1afaa115623d28983ffc5a134d320b0a58
Martin Drimal <373769@mail.muni.cz>
2014-11-02 21:33:31 +0100

    Update goods service wit DTO object

--- MultimoduleDeliveryService/ServiceLayer/src/main/java/pa165/servicelayer/serviceImplementation/GoodsServiceImpl.java
+++ MultimoduleDeliveryService/ServiceLayer/src/main/java/pa165/servicelayer/serviceImplementation/GoodsServiceImpl.java
@@ -3,45 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pa165.servicelayer.serviceImplementation;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import pa165.deliveryservice.daoImplementation.GoodsDaoImpl;
 import pa165.deliveryservice.entity.Delivery;
 import pa165.deliveryservice.entity.Goods;
+import pa165.servicelayer.dto.GoodsDto;
 import pa165.servicelayer.serviceInterface.GoodsService;
 
 /**
  *
  * @author Drimal
  */
-public class GoodsServiceImpl implements GoodsService{
+public class GoodsServiceImpl implements GoodsService {
 
     @Autowired
     private GoodsDaoImpl goodsDao;
 
+    @Autowired
+    private Mapper mapper;
+
     @Override
     public Goods createGoods(long price, String seller, Delivery delivery) {
-        if(price < 0){
+        if (price < 0) {
             throw new IllegalArgumentException("Price can't be negative.");
         }
-        if(seller.isEmpty()){
+        if (seller.isEmpty()) {
             throw new IllegalArgumentException("Seller can't be empty.");
         }
-        if(delivery == null){
+        if (delivery == null) {
             throw new NullPointerException("Delivery can't be null.");
         }
         Goods goods = new Goods();
         goods.setPrice(price);
         goods.setSeller(seller);
         goods.setDelivery(delivery);
-        try{
+        try {
             goodsDao.addGoods(goods);
-        }catch(Exception ex){
-            throw new DataAccessException("Error in persistance layer.", ex){};
+        } catch (Exception ex) {
+            throw new DataAccessException("Error in persistance layer.", ex) {
+            };
         }
         return goods;
     }
@@ -49,46 +55,53 @@ public class GoodsServiceImpl implements GoodsService{
     @Override
     public boolean deleteGoods(Goods goods) {
         boolean result = false;
-        if(goods == null){
+        if (goods == null) {
             throw new NullPointerException("Goods can't be null.");
         }
-        try{
+        try {
             goodsDao.deleteGoods(goods);
             return true;
-        }catch(Exception ex){
-            throw new DataAccessException("Error in persistance layer.", ex){};
+        } catch (Exception ex) {
+            throw new DataAccessException("Error in persistance layer.", ex) {
+            };
         }
     }
 
     @Override
     public void updateGoods(Goods goods) {
-        if(goods == null){
+        if (goods == null) {
             throw new NullPointerException("Goods can't be null.");
         }
-        try{
+        try {
             goodsDao.updateGoods(goods);
-        }catch(Exception ex){
-            throw new DataAccessException("Error in persistance layer.", ex){};
+        } catch (Exception ex) {
+            throw new DataAccessException("Error in persistance layer.", ex) {
+            };
         }
     }
 
     @Override
-    public List<Goods> getAllGoods() {
-        return goodsDao.getAllGoods();
+    public List<GoodsDto> getAllGoods() {
+        List<GoodsDto> resultList = new ArrayList<>();
+        for (Goods goods : goodsDao.getAllGoods()) {
+            resultList.add(mapper.map(goods, GoodsDto.class));
+        }
+        return resultList;
     }
 
     @Override
-    public Goods findGood(long id) {
-        if(id < 0){
+    public GoodsDto findGood(long id) {
+        if (id < 0) {
             throw new IllegalArgumentException("Id can't be negative.");
         }
         Goods goods = null;
-        try{
+        try {
             goods = goodsDao.getGoods(id);
-        }catch(Exception ex){
-            throw new DataAccessException("Error in persistance layer.", ex){};
+        } catch (Exception ex) {
+            throw new DataAccessException("Error in persistance layer.", ex) {
+            };
         }
-        return goods;
+        return mapper.map(goods, GoodsDto.class);
     }
-    
+
 }
