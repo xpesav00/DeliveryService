4731ed16b164e0d0fe3539348bd1eafc8327a632
Martin Drimal <373769@mail.muni.cz>
2014-11-23 22:30:56 +0100

    Uprava packagu a cest

--- DeliveryServiceParent/DeliveryService-Web/src/main/java/pa165/deliveryweb/config/MySpringMvcConfig.java
+++ DeliveryServiceParent/DeliveryService-Web/src/main/java/pa165/deliveryweb/config/MySpringMvcConfig.java
@@ -43,7 +43,7 @@ public class MySpringMvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         log.debug("mapping URL / to home view");
         registry.addViewController("/").setViewName("home");
-        //registry.addViewController("/snoop").setViewName("snoop");
+        registry.addViewController("/snoop").setViewName("snoop");
     }
 
 
